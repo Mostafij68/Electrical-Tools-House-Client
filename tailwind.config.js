4193cedd7de556777c8bbfdc5be3b8aeb6d1b046
Mostafij Mozumdar <mostafijmf@gmail.com>
2022-05-24 01:37:45 +0600

    basic setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#5ffcff",
+          secondary: "#00d8d6",
+          accent: "#b71540",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
